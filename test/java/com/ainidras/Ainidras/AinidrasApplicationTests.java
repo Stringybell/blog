c682d30d642ffd0255b40596a502cd7040df42d7
Stringybell <christianmartinpileri@gmail.com>
2024-05-06 08:00:03 +1000

    Spring-boot framework

---
+++ test/java/com/ainidras/Ainidras/AinidrasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ainidras.Ainidras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AinidrasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
