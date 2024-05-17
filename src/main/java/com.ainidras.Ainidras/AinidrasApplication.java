187e144c46c3bd511938ae586c6f78446499151f
Stringybell <christianmartinpileri@gmail.com>
2024-05-17 23:10:58 +1000

    H2 added

---
+++ src/main/java/com.ainidras.Ainidras/AinidrasApplication.java
@@ -0,0 +1,13 @@
+package com.ainidras.Ainidras;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AinidrasApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AinidrasApplication.class, args);
+    }
+
+}
